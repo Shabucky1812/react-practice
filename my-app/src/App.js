fa88ba22429aae8d455ca112c8768d3d787e3aeb
Shabucky1812 <shaunbuck1812@gmail.com>
2023-07-03 14:16:18 +0000

    Component did mount

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import RenderingLists from './components/RenderingLists';
+import LifeCyclesCDM from './components/LifeCyclesCDM';
 
 function App() {
   return (
     <div className="App">
-        <RenderingLists />
+        <LifeCyclesCDM />
     </div>
   );
 }
